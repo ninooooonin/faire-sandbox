8b0dcab12a5bdb94756573bfb65c5196d18c6f8b
ninooooonin <ninovillaflor1997@gmail.com>
2024-06-06 14:35:24 +0800

    Remove unused files and assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [userTypes, setUserTypes] = useState('retailers');
+    const [locale, setLocale] = useState('en');
+
+    const handleUserTypes = (key) => {
+        setUserTypes(key);
+    }
+
+    const handleLocale = (key) => {
+        setLocale(key);
+    }
+
+    return (
+        <div className="container">
+            <div className="wrapper">
+                <h1>Faire Sandbox</h1>
+                <div className="form-field">
+                    <span>User types</span>
+                    <div className="form-row">
+                        <button className={`${userTypes === 'retailers'? 'selected': ''}`}
+                            onClick={() => handleUserTypes('retailers')}>Retailers</button>
+                        <button className={`${userTypes === 'brands'? 'selected': ''}`}
+                            onClick={() => handleUserTypes('brands')}>Brands</button>
+                    </div>
+                </div>
+                <div className="form-field" style={{ marginTop: 24 }}>
+                    <span>Locales</span>
+                    <div className="form-row">
+                        <button className={`${locale === 'en'? 'selected': ''}`}
+                            onClick={() => handleLocale('en')}>en</button>
+                        <button className={`${locale === 'fr'? 'selected': ''}`}
+                            onClick={() => handleLocale('fr')}>fr</button>
+                        <button className={`${locale === 'de'? 'selected': ''}`}
+                            onClick={() => handleLocale('de')}>de</button>
+                    </div>
+                </div>
+
+                <div className="form-field" style={{ marginTop: 24 }}>
+                    <span style={{ paddingBottom: 0 }}>Local Storage Value</span>
+                    <div className="form-row">
+                        <p>English</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
