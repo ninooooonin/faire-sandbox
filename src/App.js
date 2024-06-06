9aec66968910c9ba1a12e11e57e22724e413dc4e
ninooooonin <ninovillaflor1997@gmail.com>
2024-06-06 16:09:38 +0800

    Finalize functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,94 @@
-import { useState } from 'react';
-
+import { init } from 'commandbar';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+	const [isMounted, setMount] = useState(false);
     const [userTypes, setUserTypes] = useState('retailers');
     const [locale, setLocale] = useState('en');
+    
 
-    const handleUserTypes = (key) => {
-        setUserTypes(key);
+    // Org ID: f5bf530d and ab576de6
+
+    const componentWillUnmount = () => {
+		setMount(false);
+    };
+
+    const componentDidMount = () => {
+		if(!isMounted) {
+			setMount(true);
+            setUserTypes(getLocalStorage('userTypes'));
+            setLocale(getLocalStorage('locale'));
+		}
     }
 
-    const handleLocale = (key) => {
-        setLocale(key);
+	// Life Cycle
+	useEffect(() => {
+        componentDidMount();
+        return componentWillUnmount;
+    }, []);
+
+    const handleUserTypes = (value) => {
+        setUserTypes(value);
+        setLocalStorage('userTypes', value);
+        window.location.reload();
     }
+
+    const handleLocale = (value) => {
+        setLocale(value);
+        setLocalStorage('locale', value);
+        window.location.reload();
+    }
+
+    const setLocalStorage = (key, value) => {
+        localStorage.setItem(`faire-sandbox.${key}`, value);
+    }
+
+    const getLocalStorage = (key) => {
+        return localStorage.getItem(`faire-sandbox.${key}`);
+    }
+
+    const getAllFaireSandboxEntries = () => {
+        let entries = [];
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            if (key.startsWith('faire-sandbox.')) {
+                let value = localStorage.getItem(key);
+                entries.push(`${key}: ${value}`);
+            }
+        }
+        return entries.join(', ');
+    };
+
+    useEffect(() => {
+        if (isMounted) {
+            const orgId = userTypes === 'brands' ? 'f5bf530d' : 'ab576de6';
+            init(orgId);
+            window.CommandBar.boot('sandbox-user', {});
+            window.CommandBar.toggleHelpHub();
+    
+            console.log("Active User Types:", userTypes);
+            console.log("Active Locale:", locale);
+    
+            const applyHelpHubFilter = () => {
+                switch (locale) {
+                    case 'fr':
+                        window.CommandBar.setHelpHubFilter({ labels: ["fr", "locale__fr-fr"] });
+                        console.log('Use French');
+                        break;
+                    case 'de':
+                        window.CommandBar.setHelpHubFilter({ labels: ["de", "locale_de"] });
+                        console.log('Use German');
+                        break;
+                    default:
+                        window.CommandBar.clearHelpHubFilter();
+                        console.log('Clear helphub');
+                        break;
+                }
+            };
+    
+            applyHelpHubFilter();
+        }
+    }, [isMounted]);
 
     return (
         <div className="container">
@@ -39,9 +116,9 @@ const App = () => {
                 </div>
 
                 <div className="form-field" style={{ marginTop: 24 }}>
-                    <span style={{ paddingBottom: 0 }}>Local Storage Value</span>
+                    <span>Local Storage Value</span>
                     <div className="form-row">
-                        <p>English</p>
+                        <code>{getAllFaireSandboxEntries()}</code>
                     </div>
                 </div>
 
